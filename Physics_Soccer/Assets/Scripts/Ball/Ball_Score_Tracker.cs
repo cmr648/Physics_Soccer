47422fba6b35d516ceba098e6bf36bbc960287c7
Unknown <cmr648@nyu.edu>
2018-05-16 00:42:38 -0400

    Ball can track players better

--- Physics_Soccer/Assets/Scripts/Ball/Ball_Score_Tracker.cs
+++ Physics_Soccer/Assets/Scripts/Ball/Ball_Score_Tracker.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Ball_Score_Tracker : MonoBehaviour {
 
 	GameObject Last_Object_Touched = null; // creating a gameobject reference for the last object touched
-
+    float Player_Number; // creating an int that will track the actual player number that has hit the ball
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +23,10 @@ public class Ball_Score_Tracker : MonoBehaviour {
 
 
 			Last_Object_Touched = col.gameObject; // assignign the last obejct hit to our gameobject variable
+
+            Player_Number = col.gameObject.GetComponent<Character_Assignment>().Player_Assignment_Number; // assigning our player number to our last object touched
+
+          
 		
 		}
 
